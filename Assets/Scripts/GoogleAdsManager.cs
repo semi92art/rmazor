2286a145de861f0416f1fe3fb8bb1283fdf16f64
av_smirnov <artem@desktop.com>
2020-08-13 22:31:07 +0300

    Готовим каркас

---
+++ Assets/Scripts/GoogleAdsManager.cs
@@ -0,0 +1,22 @@
+﻿using GoogleMobileAds.Api;
+
+public class AdvertisingManager
+{
+    private BannerView m_BannerAd;
+    private InterstitialAd m_FullscreenAd;
+    private RewardedAd m_RewardedAd;
+    private AdLoader m_NativeAd;
+    
+    public void InitGoogleAds()
+    {
+        CommonUtils.RegisterTestDevices();
+        MobileAds.Initialize(initStatus => { });
+        
+        m_BannerAd = new BannerView(ResourcesLoader.Instance.GoogleAdsBannerId, AdSize.SmartBanner, AdPosition.Bottom);
+        m_FullscreenAd = new InterstitialAd(ResourcesLoader.Instance.GoogleAdsFullscreenId);
+        m_RewardedAd = new RewardedAd(ResourcesLoader.Instance.GoogleAdsRewardId);
+        m_NativeAd = new AdLoader.Builder(ResourcesLoader.Instance.GoogleAdsNativeAdId).Build();
+    }
+    
+    
+}
