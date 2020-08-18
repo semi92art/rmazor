09296a146e44d09a4b390a90033ff18c20ddf391
av_chernov <av_chernov@rambler.ru>
2020-08-18 12:18:15 +0300

    [00002] Classes for UI creation

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -5,10 +5,13 @@ namespace Clickers
 {
     public class GameLoader : MonoBehaviour
     {
+        public bool isAddsOn = false;
+        public string SceneToLoad = "_menu";
         private void Start()
         {
-            GoogleAdsManager.Instance.Init();
-            SceneManager.LoadScene("Main");
+            if (this.isAddsOn)
+                GoogleAdsManager.Instance.Init();
+            SceneManager.LoadScene(this.SceneToLoad);
 
             SceneManager.activeSceneChanged += (previous, current) => { };
         }
