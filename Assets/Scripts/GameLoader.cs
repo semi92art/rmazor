34d164fa1a614dc937e174561e822d0a77e9750f
av_smirnov <artem@desktop.com>
2020-08-18 21:36:51 +0300

    [00002] UI Creation System: - недостающий файл едитора - всякие мелочи

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -1,19 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Clickers
 {
     public class GameLoader : MonoBehaviour
     {
-        public bool isAddsOn = false;
-        public string SceneToLoad = "_menu";
         private void Start()
         {
-            if (this.isAddsOn)
-                GoogleAdsManager.Instance.Init();
-            SceneManager.LoadScene(this.SceneToLoad);
-
-            SceneManager.activeSceneChanged += (previous, current) => { };
+            GoogleAdsManager.Instance.Init();
+            SceneManager.activeSceneChanged += (previous, current) =>
+            {
+                Canvas canvas = UIFactory.UICanvas(
+                    "0",
+                    RenderMode.ScreenSpaceOverlay,
+                    true,
+                    0,
+                    AdditionalCanvasShaderChannels.None,
+                    CanvasScaler.ScaleMode.ScaleWithScreenSize,
+                    Vector2Int.one,
+                    CanvasScaler.ScreenMatchMode.Shrink,
+                    0f,
+                    100,
+                    true,
+                    GraphicRaycaster.BlockingObjects.None);
+                
+                UIFactory.UIImage(UIFactory.UIRectTransform(
+                    canvas.rectTransform(),
+                    "image",
+                    UIAnchor.Create(0, 0, 0, 0),
+                    Vector2.down, Vector2.down, Vector2.down
+                    ), "TESTButton");
+            };
+            
+            SceneManager.LoadScene("Menu");
         }
     }
 }
