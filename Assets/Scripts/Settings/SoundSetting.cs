066376c337fd8fc7cc9517bfc00b8add2c08abe1
av_smirnov <artem@desktop.com>
2020-10-15 20:05:16 +0300

    нормальный gitignore = подтянулась папка Package и еще всякие мелочи

---
+++ Assets/Scripts/Settings/SoundSetting.cs
@@ -0,0 +1,9 @@
+﻿public class SoundSetting : ISetting
+{
+    public void Set(object _Parameter)
+    {
+        bool volumeOn = (bool) _Parameter;
+        
+        //TODO set sound on/off
+    }
+}
