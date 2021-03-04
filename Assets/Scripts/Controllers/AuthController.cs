50b24845e84ad2ff616aa435dd5f1b3c3e68aa63
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2021-03-04 07:33:18 +0300

    еще 1 пример интеграции Game Center на iOS

--- Assets/Scripts/Controllers/AuthController.cs
+++ Assets/Scripts/Controllers/AuthController.cs
@@ -182,7 +182,50 @@ namespace Controllers
     }
 }
 
-//Apple Game Center integration example
+//--------------------Apple Game Center integration example 1------------------
+
+// using UnityEngine;
+// using UnityEngine.SocialPlatforms;
+//
+// public class SocialExample : MonoBehaviour {
+//     
+//     void Start () {
+//         // Authenticate and register a ProcessAuthentication callback
+//         // This call needs to be made before we can proceed to other calls in the Social API
+//         Social.localUser.Authenticate (ProcessAuthentication);
+//     }
+//
+//     // This function gets called when Authenticate completes
+//     // Note that if the operation is successful, Social.localUser will contain data from the server. 
+//     void ProcessAuthentication (bool success) {
+//         if (success) {
+//             Debug.Log ("Authenticated, checking achievements");
+//
+//             // Request loaded achievements, and register a callback for processing them
+//             Social.LoadAchievements (ProcessLoadedAchievements);
+//         }
+//         else
+//             Debug.Log ("Failed to authenticate");
+//     }
+//
+//     // This function gets called when the LoadAchievement call completes
+//     void ProcessLoadedAchievements (IAchievement[] achievements) {
+//         if (achievements.Length == 0)
+//             Debug.Log ("Error: no achievements found");
+//         else
+//             Debug.Log ("Got " + achievements.Length + " achievements");
+//      
+//         // You can also call into the functions like this
+//         Social.ReportProgress ("Achievement01", 100.0, result => {
+//             if (result)
+//                 Debug.Log ("Successfully reported achievement progress");
+//             else
+//                 Debug.Log ("Failed to report achievement");
+//         });
+//     }
+// }
+
+//-----------------Apple Game Center integration example 2------------------
 
 // using UnityEngine;
 // using System.Collections;
