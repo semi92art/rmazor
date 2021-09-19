b1361b1789b3f199efe733159320806b2727db24
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2021-09-19 09:15:31 +0300

    Ускорение загрузки уровней - первого в ~20 раз, второго и последующих в ~400 раз.

--- Client/Assets/Scripts/GameHelpers/LevelsLoader.cs
+++ Client/Assets/Scripts/GameHelpers/LevelsLoader.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using GameHelpers;
+﻿using System;
+using System.Linq;
+using Extensions;
 using Games.RazorMaze.Models;
 using Newtonsoft.Json;
 using UnityEngine;
 
-namespace Games.RazorMaze
+namespace GameHelpers
 {
     public interface ILevelsLoader
     {
@@ -17,19 +17,27 @@ namespace Games.RazorMaze
         private const int GroupsInAsset = 50;
 
         private int m_GameId;
-        private List<MazeInfo> m_CachedList;
+        private string[] m_CachedSerializedLevels;
         
-
         public MazeInfo LoadLevel(int _GameId, int _Index, bool _FromBundle)
         {
-            if (m_CachedList != null && m_GameId == _GameId)
-                return m_CachedList[_Index];
+            if (m_CachedSerializedLevels != null && m_GameId == _GameId)
+                return JsonConvert.DeserializeObject<MazeInfo>(m_CachedSerializedLevels[_Index]);
             m_GameId = _GameId;
             var asset = PrefabUtilsEx.GetObject<TextAsset>(PrefabSetName(_GameId),
                 LevelsAssetName(1), _FromBundle);
-            var levelsListRaw = JsonConvert.DeserializeObject<MazeLevelsList>(asset.text);
-            m_CachedList = levelsListRaw.Levels;
-            return m_CachedList[_Index];
+            var t = typeof(MazeInfo);
+            var firstProp = t.GetProperties()[0];
+            var levelsText = asset.text;
+            levelsText = levelsText.Remove(levelsText.Length - 2, 2);
+            string splitter = "{" + "\"" + firstProp.Name + "\"";
+            var serializedLevels = levelsText.Split(new []{splitter, "," + splitter}, StringSplitOptions.None);
+            serializedLevels = serializedLevels
+                .RemoveRange(new[] {serializedLevels[0]})
+                .Select(_MazeSerialized => splitter + _MazeSerialized).ToArray();
+            var mazeInfo = JsonConvert.DeserializeObject<MazeInfo>(serializedLevels[_Index]);
+            m_CachedSerializedLevels = serializedLevels;
+            return mazeInfo;
         }
         
         protected static string PrefabSetName(int _GameId) => $"game_{_GameId}_levels";
