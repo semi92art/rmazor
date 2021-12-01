00668c8149d7498083d9747595e004be70bbb6c8
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2021-12-01 18:48:36 +0300

    Шифрование PlayerPref-ов на устройстве

--- Client/Assets/Scripts/Utils/SaveUtils.cs
+++ Client/Assets/Scripts/Utils/SaveUtils.cs
@@ -6,11 +6,16 @@ namespace Utils
 {
     public static class SaveUtils
     {
+        private const string PassPhrase = "f47759941904a9bf6f89736c4541d850107c9be6ec619e7e65cf80a14ff7e8e4";
+        
         public static T GetValue<T>(SaveKey<T> _Key)
         {
             string value = PlayerPrefs.GetString(_Key.Key, string.Empty);
             if (string.IsNullOrEmpty(value))
                 return default;
+#if !UNITY_EDITOR
+            value = StringCipher.Decrypt(value, PassPhrase);
+#endif
             var result = JsonConvert.DeserializeObject<T>(value, SerializerSettings);
             return result;
         }
@@ -18,6 +23,9 @@ namespace Utils
         public static void PutValue<T>(SaveKey<T> _Key, T _Value)
         {
             string value = JsonConvert.SerializeObject(_Value, SerializerSettings);
+#if !UNITY_EDITOR
+            value = StringCipher.Encrypt(value, PassPhrase);
+#endif
             PlayerPrefs.SetString(_Key.Key, value);
             PlayerPrefs.Save();
         }
