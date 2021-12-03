4ee8170707c9f89c1a56a5865d78dbf33346fff1
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2021-12-03 12:08:47 +0300

    в догонку к f00beaf3

---
+++ Client/Assets/Scripts/Controllers/AudioClipArgs.cs
@@ -0,0 +1,31 @@
+﻿namespace Controllers
+{
+    public class AudioClipArgs
+    {
+        public string         ClipName                 { get; }
+        public EAudioClipType Type                     { get; }
+        public string         Id                       { get; }
+        public bool           Loop                     { get; set; }
+        public float          StartVolume              { get; set; }
+        public float          AttenuationSecondsOnPlay { get; set; }
+        public float          AttenuationSecondsOnStop { get; set; }
+
+        public AudioClipArgs(
+            string _ClipName,
+            EAudioClipType _Type,
+            float _Volume = 1f, 
+            bool _Loop = false, 
+            string _Id = null,
+            float _AttenuationSecondsOnPlay = 0f,
+            float _AttenuationSecondsOnStop = 0f)
+        {
+            ClipName = _ClipName;
+            Type = _Type;
+            StartVolume = _Volume;
+            Loop = _Loop;
+            Id = _Id;
+            AttenuationSecondsOnPlay = _AttenuationSecondsOnPlay;
+            AttenuationSecondsOnStop = _AttenuationSecondsOnStop;
+        }
+    }
+}
