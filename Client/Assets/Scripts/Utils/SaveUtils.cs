1cacf6e7b00d019c88a6fb994ce3b92d65f08c55
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2021-12-08 10:14:26 +0300

    для поддержки сохранения в неосновных потоках вместо PlayerPrefs теперь используем файл в Application.persistentDataPath

--- Client/Assets/Scripts/Utils/SaveUtils.cs
+++ Client/Assets/Scripts/Utils/SaveUtils.cs
@@ -1,4 +1,6 @@
-﻿using Entities;
+﻿using System.IO;
+using System.Xml.Linq;
+using Entities;
 using Newtonsoft.Json;
 using UnityEngine;
 
@@ -6,11 +8,24 @@ namespace Utils
 {
     public static class SaveUtils
     {
-        private const string PassPhrase = "f47759941904a9bf6f89736c4541d850107c9be6ec619e7e65cf80a14ff7e8e4";
+        private const           string    PassPhrase = "f47759941904a9bf6f89736c4541d85";
+        private static readonly string    SavesPath  = Path.Combine(Application.persistentDataPath, "Saves.xml");
+        private static          XDocument _savesDoc;
+
+        static SaveUtils()
+        {
+            CreateSavesFileIfNotExist();
+        }
+        
+        private static JsonSerializerSettings SerializerSettings => new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            NullValueHandling = NullValueHandling.Include
+        };
         
         public static T GetValue<T>(SaveKey<T> _Key)
         {
-            string value = PlayerPrefs.GetString(_Key.Key, string.Empty);
+            string value = GetXElementValue(_Key.Key);
             if (string.IsNullOrEmpty(value))
                 return default;
 #if !UNITY_EDITOR
@@ -26,14 +41,46 @@ namespace Utils
 #if !UNITY_EDITOR
             value = StringCipher.Encrypt(value, PassPhrase);
 #endif
-            PlayerPrefs.SetString(_Key.Key, value);
-            PlayerPrefs.Save();
+            PutXElementValue(_Key.Key, value);
         }
 
-        private static JsonSerializerSettings SerializerSettings => new JsonSerializerSettings
+        private static void CreateSavesFileIfNotExist()
         {
-            TypeNameHandling = TypeNameHandling.Auto,
-            NullValueHandling = NullValueHandling.Include
-        };
+            if (File.Exists(SavesPath))
+            {
+                _savesDoc = XDocument.Load(SavesPath);
+                return;
+            }
+            _savesDoc = new XDocument(
+                new XComment("This file contains encrypted game data."),
+                new XElement("data"));
+            _savesDoc.Save(SavesPath);
+        }
+
+        private static string GetXElementValue(string _Key)
+        {
+            var rootEl = _savesDoc.Element("data");
+            return rootEl?.Element(_Key)?.Value;
+        }
+
+        private static void PutXElementValue(string _Key, string _Value)
+        {
+            var rootEl = _savesDoc.Element("data");
+            if (rootEl == null)
+            {
+                Dbg.LogError($"{nameof(PutXElementValue)}: Root element is null");
+                return;
+            }
+            var el = rootEl.Element(_Key);
+            if (el == null)
+            {
+                rootEl.Add(new XElement(_Key, _Value));
+                _savesDoc.Save(SavesPath);
+                return;
+            }
+            el.Value = _Value;
+            
+            _savesDoc.Save(SavesPath, SaveOptions.None);
+        }
     }
 }
