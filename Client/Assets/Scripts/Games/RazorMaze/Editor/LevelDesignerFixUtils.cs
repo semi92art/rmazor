8e6de84cd876dd60d6289ecc5c782f8602b33c9f
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2021-12-21 11:14:46 +0300

    новые fixutils для левелдизайнера

--- Client/Assets/Scripts/Games/RazorMaze/Editor/LevelDesignerFixUtils.cs
+++ Client/Assets/Scripts/Games/RazorMaze/Editor/LevelDesignerFixUtils.cs
@@ -2,30 +2,12 @@
 using System.Linq;
 using Entities;
 using Games.RazorMaze.Models;
-using UnityEditor;
+using Utils;
 
 namespace Games.RazorMaze.Editor
 {
     public partial class LevelDesignerEditor
     {
-        // [FixUtil]
-        // public static void FixPaths()
-        // {
-        //     var levels = LevelsList.Levels.ToList();
-        //     int k = 0;
-        //     var last = levels.Last();
-        //     foreach (var lev in levels)
-        //     {
-        //         float progress = (float)k / levels.Count;
-        //         lev.Path = lev.Path.Distinct().ToList();
-        //         foreach (var mazeItem in lev.MazeItems.Where(_Item => _Item.Path.Any()))
-        //             mazeItem.Path = mazeItem.Path.Distinct().ToList();
-        //         EditorUtility.DisplayProgressBar("Fixing paths", $"Fixing paths: {k + 1}/{levels.Count}", progress);
-        //         LevelsList.Save();
-        //     }
-        //     EditorUtility.ClearProgressBar();
-        // }
-
         [FixUtil]
         public static void FixTrapsReact()
         {
@@ -57,6 +39,116 @@ namespace Games.RazorMaze.Editor
                 }
             }
             LevelsList.Save();
+        }
+
+        [FixUtil]
+        public static void FindLevelsWithNonPairPortals()
+        {
+            bool errors = false;
+            var levels = LevelsList.Levels;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                var l = levels[i];
+                var portals = l.MazeItems
+                    .Where(_I => _I.Type == EMazeItemType.Portal)
+                    .ToList();
+                if (!portals.Any())
+                    continue;
+                if (portals.All(_I => _I.Pair != default))
+                    continue;
+                Dbg.LogError($"Level {i + 1} has invalid portals.");
+                errors = true;
+            }
+            if (!errors)
+                Dbg.Log("All portals are valid.");
+        }
+
+        [FixUtil]
+        public static void FindLevelsWithTurretsInvalid()
+        {
+            bool errors = false;
+            var levels = LevelsList.Levels;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                var l = levels[i];
+                var turrets = l.MazeItems
+                    .Where(_I => _I.Type == EMazeItemType.Turret)
+                    .ToList();
+                if (!turrets.Any())
+                    continue;
+                if (!turrets.Any(_I => !_I.Directions.Any() || _I.Directions.First() == default))
+                    continue;
+                Dbg.LogError($"Level {i + 1} has invalid turrets.");
+                errors = true;
+            }
+            if (!errors)
+                Dbg.Log("All turrets are valid.");
+        }
+        
+        [FixUtil]
+        public static void FindLevelsWithReactsTrapsInvalid()
+        {
+            bool errors = false;
+            var levels = LevelsList.Levels;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                var l = levels[i];
+                var turrets = l.MazeItems
+                    .Where(_I => _I.Type == EMazeItemType.TrapReact)
+                    .ToList();
+                if (!turrets.Any())
+                    continue;
+                if (!turrets.Any(_I => !_I.Directions.Any() || _I.Directions.Any(_D => _D == default))) 
+                    continue;
+                Dbg.LogError($"Level {i + 1} has invalid react traps.");
+                errors = true;
+            }
+            if (!errors)
+                Dbg.Log("All trap reacts are valid.");
+        }
+        
+        [FixUtil]
+        public static void FindLevelsWithMovingTrapsInvalid()
+        {
+            bool errors = false;
+            var levels = LevelsList.Levels;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                var l = levels[i];
+                var turrets = l.MazeItems
+                    .Where(_I => _I.Type == EMazeItemType.TrapMoving)
+                    .ToList();
+                if (!turrets.Any())
+                    continue;
+                if (!turrets.Any(_I => !_I.Path.Any() || _I.Path.Count != _I.Path.Distinct().Count())) 
+                    continue;
+                Dbg.LogError($"Level {i + 1} has invalid moving traps.");
+                errors = true;
+            }
+            if (!errors)
+                Dbg.Log("All moving traps are valid.");
+        }
+        
+        [FixUtil]
+        public static void FindLevelsWithGravityBlocksInvalid()
+        {
+            bool errors = false;
+            var levels = LevelsList.Levels;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                var l = levels[i];
+                var turrets = l.MazeItems
+                    .Where(_I => _I.Type == EMazeItemType.GravityBlock)
+                    .ToList();
+                if (!turrets.Any())
+                    continue;
+                if (!turrets.Any(_I => !_I.Path.Any() || _I.Path.Count != _I.Path.Distinct().Count())) 
+                    continue;
+                Dbg.LogError($"Level {i + 1} has invalid gravity blocks.");
+                errors = true;
+            }
+            if (!errors)
+                Dbg.Log("All gravity blocks are valid.");
         }
     }
 }
