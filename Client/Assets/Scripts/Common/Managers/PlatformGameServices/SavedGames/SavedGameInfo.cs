fee1cbdc58cfd1ea6a13e4c55a2d855ba0088be4
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2023-04-21 12:30:42 +0300

    Revert "выпилил Firebase, GoogleMobileAds, GooglePlayGames"
    This reverts commit df73671bc360abe355774fa91265eba504732744.
    
    # Conflicts:
    #	Client/Assets/Scripts/Common/IFirebaseInitializer.cs
    #	Client/Assets/Scripts/Common/Managers/Notifications/IPushNotificationsProvider.cs
    #	Client/Assets/Scripts/RMAZOR/Managers/IRemoteConfigProvider.cs

---
+++ Client/Assets/Scripts/Common/Managers/PlatformGameServices/SavedGames/SavedGameInfo.cs
@@ -0,0 +1,13 @@
+﻿using Common.Entities;
+using GooglePlayGames.BasicApi.SavedGame;
+using mazing.common.Runtime.Entities;
+
+namespace Common.Managers.PlatformGameServices.SavedGames
+{
+    public class SavedGameInfo
+    {
+        public byte[]             Data     { get; set; }
+        public ISavedGameMetadata MetaData { get; set; }
+        public Entity<object>     Entity   { get; set; }
+    }
+}
